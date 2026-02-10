a520b0557c6e0211c7d03342926c4e3b3ce9be22
Nathan Wolke <industry86@hotmail.com>
2026-02-09 22:51:54 -0800

    removed legacy auth and old npccontroller

--- GM_Buddy.Contracts/AuthModels/Entities/Role.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GM_Buddy.Contracts.AuthModels.Entities;
-public record Role
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Description { get; set; }
-}
--- GM_Buddy.Contracts/Interfaces/IAuthObjectResolver.cs
+++
@@ -1,7 +0,0 @@
-﻿using GM_Buddy.Contracts.AuthModels.Responses;
-
-namespace GM_Buddy.Contracts.Interfaces;
-public interface IAuthObjectResolver
-{
-    Task<ProfileResponse?> GetUserProfile(string email);
-}
